f8b19136904a426d74e5fecce8d204fd4f5c6699
CEJSH <eun2763@naver.com>
2025-06-05 12:58:40 +0900

    test: 스냅샷 테스트

--- src/pages/cart/components/tests/PageTitle.spec.jsx
+++ src/pages/cart/components/tests/PageTitle.spec.jsx
@@ -3,6 +3,26 @@ import React from 'react';
 import PageTitle from '@/pages/cart/components/PageTitle';
 import render from '@/utils/test/render';
 
-it('pageTitle 스냅샷 테스트(toMatchInlineSnapshot)', async () => {});
+// container 프로퍼티 -> 렌더링된 돔 구조를 div블록으로 감싼 결과를 얻을 수 있음
+// 테스트 파일 내에 스냅샷까지 함께 관리하고 싶을 경우
+it('pageTitle 스냅샷 테스트(toMatchInlineSnapshot)', async () => {
+  const { container } = await render(<PageTitle />);
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <h1
+        class="MuiTypography-root MuiTypography-h4 css-1lnl64-MuiTypography-root"
+      >
+        상품 리스트
+      </h1>
+      <div
+        style="position: fixed; z-index: 9999; top: 16px; left: 16px; right: 16px; bottom: 16px; pointer-events: none;"
+      />
+    </div>
+  `);
+});
 
-it('pageTitle 스냅샷 테스트(toMatchSnapshot)', async () => {});
+// 테스트 파일과 스냅샷 파일을 별도로 관리하고 싶은 경우
+it('pageTitle 스냅샷 테스트(toMatchSnapshot)', async () => {
+  const { container } = await render(<PageTitle />);
+  expect(container).toMatchSnapshot();
+});
